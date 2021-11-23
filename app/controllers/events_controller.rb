3337ef697e452ab3303bff27cfd178ee8c130747
Philipp Obrivanov <milliardex@gmail.com>
2021-11-23 14:00:06 +0300

    users initial

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,25 +1,21 @@
 class EventsController < ApplicationController
   before_action :set_event, only: %i[ show edit update destroy ]
 
-  # GET /events or /events.json
   def index
     @events = Event.all
   end
 
-  # GET /events/1 or /events/1.json
   def show
   end
 
-  # GET /events/new
   def new
     @event = Event.new
   end
 
-  # GET /events/1/edit
+
   def edit
   end
 
-  # POST /events or /events.json
   def create
     @event = Event.new(event_params)
 
@@ -34,7 +30,6 @@ class EventsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /events/1 or /events/1.json
   def update
     respond_to do |format|
       if @event.update(event_params)
@@ -47,7 +42,6 @@ class EventsController < ApplicationController
     end
   end
 
-  # DELETE /events/1 or /events/1.json
   def destroy
     @event.destroy
     respond_to do |format|

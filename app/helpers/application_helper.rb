b0569129c88f1b5e350f4cfc9a07912e6fd183c9
Philipp Obrivanov <milliardex@gmail.com>
2021-11-25 01:52:57 +0300

    customize flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def user_avatar(user)
     asset_path('user.png')
   end
+
+  def flash_class_name(name)
+    case name
+    when 'notice' then 'success'
+    when 'alert'  then 'danger'
+    else name
+    end
+  end
 end

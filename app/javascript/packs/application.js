eb1493c363a049711ff27711f8d7f9704905ed6c
Philipp Obrivanov <milliardex@gmail.com>
2021-11-25 01:04:00 +0300

    connect JS bootstrap file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import 'packs/scripts/bootstrap'
 
 import Rails from "@rails/ujs"
-Rails.start()
 
-import 'bootstrap/dist/js/bootstrap'
-import 'bootstrap/dist/css/bootstrap'
-import 'stylesheets/application'
+Rails.start()

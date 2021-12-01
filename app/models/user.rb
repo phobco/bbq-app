6279e543aaacb6a8df7fec18c09f639216e42f0e
Philipp Obrivanov <milliardex@gmail.com>
2021-12-01 13:17:56 +0300

    Create comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :events, dependent: :destroy
+  has_many :comments
 
   validates :name, presence: true, length: { maximum: 35 }
 end

59be70a2f9eeeb9866798aed2484facd4da2a80c
Philipp Obrivanov <milliardex@gmail.com>
2021-12-03 23:26:16 +0300

    user cannot subscribe their own event

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -4,7 +4,7 @@ class SubscriptionsController < ApplicationController
 
   def create
     @new_subscription = @event.subscriptions.build(subscription_params)
-    @new_subscription.user = current_user
+    @new_subscription.user = current_user unless @event.user == current_user
 
     if @new_subscription.save
       redirect_to @event, notice: t('controllers.subscriptions.created')

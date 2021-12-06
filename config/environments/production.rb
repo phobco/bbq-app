8eb37a766bc8b699dd152946a87c5b154cc5b6cb
Philipp Obrivanov <milliardex@gmail.com>
2021-12-06 18:44:32 +0300

    Change sendgrid to self email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,16 +112,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {host: 'meet-ing-bbq.herokuapp.com'}
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
 
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASS'], 
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
 end

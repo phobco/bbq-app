4b067154cc2ca5cf19c14661d2daf1724e797bd9
Philipp Obrivanov <milliardex@gmail.com>
2021-12-07 00:21:36 +0300

    Photo author doesn't recieve notification

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -46,7 +46,7 @@ class PhotosController < ApplicationController
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
 
     all_emails.each do |mail|
-      EventMailer.photo(event, photo, mail).deliver_now
+      EventMailer.photo(event, photo, mail).deliver_now unless photo.user.email == mail
     end
   end
 end

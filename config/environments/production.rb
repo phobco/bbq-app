affcbe9acdf7f8ba0e0adb253d09de960391d426
Philipp Obrivanov <milliardex@gmail.com>
2021-12-08 01:38:53 +0300

    Off delivery erros

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,7 +111,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = { host: 'meet-ing-bbq.herokuapp.com' }
 
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {

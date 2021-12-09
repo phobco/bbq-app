372348aca5b7bd82568bd4b7d40c109efbbdb949
Philipp Obrivanov <milliardex@gmail.com>
2021-12-09 15:58:41 +0300

    Refactor

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -42,10 +42,11 @@ class CommentsController < ApplicationController
   end
 
   def notify_subscribers(event, comment)
-    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+    all_emails = 
+      (event.subscriptions.map(&:user_email) + [event.user.email]).uniq - [comment&.user&.email]
 
     all_emails.each do |mail|
-      EventMailer.comment(event, comment, mail).deliver_now unless comment&.user&.email == mail
+      EventMailer.comment(event, comment, mail).deliver_now
     end
   end
 end

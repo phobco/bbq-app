bd5fc9cc9130ffa14ec215f696b1ca11eb1f5f68
Philipp Obrivanov <milliardex@gmail.com>
2021-12-11 00:53:30 +0300

    Change bootstrap js source

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import 'packs/scripts/bootstrap'
+import 'bootstrap/dist/js/bootstrap'
 import 'packs/scripts/map'
 
 import Rails from "@rails/ujs"

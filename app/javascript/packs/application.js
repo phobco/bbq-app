956c7dfc6acafcaa310244d8a898a700d9ed585d
Philipp Obrivanov <milliardex@gmail.com>
2021-12-10 16:51:22 +0300

    Add Yandex maps

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 import 'packs/scripts/bootstrap'
+import 'packs/scripts/map'
 
 import Rails from "@rails/ujs"
 

4f8c17184ad39b2422d2f4dbcdb784b6ab8953fe
Philipp Obrivanov <milliardex@gmail.com>
2021-12-13 03:08:35 +0300

    Add lightbox2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 import 'bootstrap/dist/js/bootstrap'
+import 'lightbox2/dist/js/lightbox'
 
 import Rails from "@rails/ujs"
 

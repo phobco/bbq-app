aadfe02634095999b02cd7e5ade6a7b67ce71dd1
Philipp Obrivanov <milliardex@gmail.com>
2022-01-14 01:12:49 +0300

    Ready for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-set :application, "BBQ"
+set :application, 'bbq'
 set :repo_url, "git@github.com:phobco/bbq.git"
 
-set deploy_to, '/home/deploy/bbq'
+set :deploy_to, '/home/deploy/bbq'
 
 append :linked_files, "config/master.key", "config/database.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "public/uploads"

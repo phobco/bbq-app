91d62aa6b2b7608585c97eb6f241669f02311a1a
Philipp Obrivanov <milliardex@gmail.com>
2022-01-18 20:44:10 +0300

    Limit title and description length

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,7 +6,8 @@ class Event < ApplicationRecord
   has_many :subscribers, through: :subscriptions, source: :user
   has_many :photos, dependent: :destroy
 
-  validates :title, presence: true, length: { maximum: 255 }
+  validates :title, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1200 }
   validates :address, :datetime, presence: true
 
   def visitors

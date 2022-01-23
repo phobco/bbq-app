a37917f9691942008ed49009f2042f6c60225fc4
Philipp Obrivanov <milliardex@gmail.com>
2022-01-23 16:05:44 +0300

    Refactor

--- app/jobs/event_email_notification_job.rb
+++ app/jobs/event_email_notification_job.rb
@@ -3,11 +3,15 @@ class EventEmailNotificationJob < ApplicationJob
 
   def perform(object)
     event = object.event
-    mailer_method = object.class.to_s.downcase
-
+    
     all_emails = 
       (event.subscriptions.map(&:user_email) + [event.user.email]).uniq - [object&.user&.email]
-    
-    all_emails.each { |mail| EventMailer.method(mailer_method).call(event, object, mail).deliver_later }
+
+    case object
+    when Comment
+      all_emails.each { |mail| EventMailer.comment(event, object, mail).deliver_later }
+    when Photo
+      all_emails.each { |mail| EventMailer.photo(event, object, mail).deliver_later }
+    end
   end
 end
